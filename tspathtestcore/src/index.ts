655d2947d66fd010085aecec327c98bbf1b8b7a3
JIIOryo <r-kawaguchi-m4w@eagle.sophia.ac.jp>
2022-03-23 10:28:01 +0900

    use tsc-alias

--- tspathtestcore/src/index.ts
+++ tspathtestcore/src/index.ts
@@ -1,5 +1,8 @@
 import * as z from './x/y/z'
 
+export const coreZ = z.z
+export const corezAdd = z.zAdd
+
 console.log(z.z)
 const A = 1
 const B = 2
--- web/src/index.ts
+++ web/src/index.ts
@@ -1,5 +1,11 @@
 import * as webLib from '@Lib/web_lib'
+import * as core from 'tspathtestcore'
 
 webLib.webLibModule()
-
 console.log(webLib.webLibMessage)
+
+// core test
+
+console.log(core.corezAdd(1, 2))
+console.log(core.coreZ)
+
